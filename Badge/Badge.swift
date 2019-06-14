9bfb1dd053507fd5b9a7f579f36d92e8fc8956c2
aning.pan <aning.pan@broadlink.com.cn>
2019-06-14 17:13:25 +0800

    add draw

--- Badge/Badge.swift
+++ Badge/Badge.swift
@@ -10,16 +10,34 @@
 import SwiftUI
 
 struct Badge: View {
-    var body: some View {
-        Path { path in
-            var width: CGFloat = 100.0
-            let height = width
-            path.move(to: CGPoint(x: width * 0.95, y: height * 0.20 ))
-            
-            HexagonParameters.points.forEach {
-                path.addLine(to: .init(x: width * $0.useWidth * $0.xFactors.0, y: height * $0.useHeight * $0.yFactors.0))
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0)
             }
-        }
-        .fill(Color.black)
+            .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+            }
+            .scaledToFit()
     }
 }
+
+#if DEBUG
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
+#endif
---
+++ Badge/BadgeSymbol.swift
@@ -0,0 +1,65 @@
+//
+//  BadgeSymbol.swift
+//  Badge
+//
+//  Created by apple on 2019/6/14.
+//  Copyright © 2019 Pszertlek. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+        .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct BadgeSymbol: View {
+    static let symbolColor = Color(red: 79.0 / 255.0, green: 79.0 / 255.0, blue: 191.0 / 255.0)
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width / 2
+                let topWidth = 0.226 * width
+                let topHeight = 0.488 * height
+                
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                    ])
+                
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                    ])
+            }
+            .fill(Self.symbolColor)
+        }
+    }
+}
+
+#if DEBUG
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
+
+
+#endif
+
